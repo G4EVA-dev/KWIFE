fb6958f8e00a6143b0cd8b920fb714063e6eed23
rachmi00 <r.agyingi@gmail.com>
2024-08-26 12:19:35 +0100

    Home page partial changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import SearchBar from '../components/Searchbar'
 import CategoryList from '../components/CategoryList'
 import Footer from '../components/Footer'
+import Popular from '../components/Popular'
 
 function  Home  (){
   return (
     <div>
         <SearchBar/>
         <CategoryList/>
+        <Popular/>
         <Footer/>
     </div>
   )
