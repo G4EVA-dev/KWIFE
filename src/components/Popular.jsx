97d1a45680e93b0a46c88f1d71df14a3d6726bc5
G4EVA-dev <tanzeglenn@gmail.com>
2024-08-26 14:49:40 +0100

    recent game view

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { ARTISTS } from '../ARTISTS';
-import ArtistCard from './ArtistCard';
+import { ARTISTS } from "../artists";
+import ArtistCard from "./ArtistCard";
 
 function Popular() {
   const popularArtists = ARTISTS.artists.slice(0, 4);
 
   return (
-    <div className="overflow-x-auto whitespace-nowrap py-4 px-2" style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
-      <div className="flex space-x-4" style={{ paddingBottom: '20px' }}>
+    <div
+      className="overflow-x-auto whitespace-nowrap py-4 px-2"
+      style={{ scrollbarWidth: "none", msOverflowStyle: "none" }}
+    >
+      <div className="flex space-x-4" style={{ paddingBottom: "20px" }}>
         {popularArtists.map((artist) => (
           <ArtistCard key={artist.id} artist={artist} />
         ))}
