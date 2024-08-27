ae14769a1feaa8a9712c5aa28e08d067bbd92550
rachmi00 <r.agyingi@gmail.com>
2024-08-27 07:04:12 +0100

    minor changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import { motion } from "framer-motion";
 import { Settings, LogOut, Plus, ToggleLeft, ToggleRight, Music, Video } from "lucide-react";
-import { Settings, LogOut, Plus, ToggleLeft, ToggleRight } from "lucide-react";
+
 import { Link } from "react-router-dom";
 
 
