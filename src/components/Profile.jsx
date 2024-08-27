ed46e6b2eaedca81ee36d8dfe2bbd1149a443985
G4EVA-dev <tanzeglenn@gmail.com>
2024-08-27 07:03:26 +0100

    test

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -11,7 +11,7 @@ const PersonalAccountPage = () => {
 
   const user = state?.artist || {
     name: "Acha Nfon",
-    email: "acha.nfon@example.com",
+    email: "acha.nfonn@example.com",
     profilePic: "/path/to/profile-pic.jpg",
     favoriteArtists: ["Mballa Elanga", "Richard Bona", "Manu Dibango"],
     recentlyPlayed: [
