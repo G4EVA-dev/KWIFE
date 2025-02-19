98374a85a92a9b57f3da1ecd9eab5571ee43a369
G4EVA-dev <tanzeglenn@gmail.com>
2025-02-19 05:17:09 +0100

    feat: include log for troubleshooting

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 const domain = import.meta.env.VITE_AUTH0_DOMAIN;
 const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
+if (!domain || !clientId) {
+  console.error("Auth0 domain or clientId is missing!");
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Auth0Provider
@@ -14,10 +18,10 @@ createRoot(document.getElementById("root")).render(
       clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin,
+        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
       }}
     >
       <App />
     </Auth0Provider>
-    s
   </StrictMode>
 );
